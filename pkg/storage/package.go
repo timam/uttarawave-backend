2b5bb59790241dcd28f747441b03fe8580169d8d
timam <mail@timam.io>
2024-09-26 23:05:24 +0600

    Remove deprecated package loading code
    The `LoadLatestPackages` function and its associated variables in `pkg/repos/package.go` have been deleted. All necessary package data will now be managed from `cmd/server/main.go`, ensuring a centralized data loading process.

--- pkg/storage/package.go
+++ pkg/storage/package.go
@@ -4,4 +4,4 @@ import (
 	"github.com/timam/uttaracloud-finance-backend/pkg/models"
 )
 
-var LoadedPackages []models.Package
+var LoadedPackages []models.Package //loading data from cmd/server/main.go
