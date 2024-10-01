0e350ed2dd632615223e6b877b7f047ef618e827
timam <mail@timam.io>
2024-10-02 01:00:54 +0600

    Switch to zap logging library
    Replace std log with zap for better logging capabilities. Zap provides more structured logging, which will make it easier to filter and parse logs.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,19 @@
 package server
 
 import (
+	"github.com/timam/uttaracloud-finance-backend/pkg/logger"
 	"github.com/timam/uttaracloud-finance-backend/routers"
-	"log"
+	"go.uber.org/zap"
 )
 
 func StartServer() {
 
 	router := routers.InitRouter()
-	log.Println("Starting server...")
 	err := router.Run(":8080")
 	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+		logger.Error("Failed to start server: %v", zap.Error(err))
 	}
+
+	logger.Info("Server has been started")
 
 }
