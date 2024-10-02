0b1fd2b643763d9f64eb4dfb7a4ea24b1bff52a5
timam <mail@timam.io>
2024-10-02 13:32:44 +0600

    Enable configurable request logging based on debug mode
    Added a `debug` flag in the server configuration to control request and response logging. The `CustomLogger` middleware now checks this flag to conditionally log request details, improving flexibility and performance in non-debug environments.

--- middlewares/sentinel.go
+++ middlewares/sentinel.go
@@ -5,29 +5,33 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"github.com/timam/uttaracloud-finance-backend/pkg/logger"
 	"go.uber.org/zap"
 )
 
-// responseWriter is a custom wrapper for gin.ResponseWriter
 type responseWriter struct {
 	gin.ResponseWriter
 	body *bytes.Buffer
 }
 
-// Write captures the response body
-func (w *responseWriter) Write(b []byte) (int, error) {
+func (w responseWriter) Write(b []byte) (int, error) {
 	w.body.Write(b)
 	return w.ResponseWriter.Write(b)
 }
 
 func CustomLogger() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		if !viper.GetBool("server.debug") {
+			c.Next()
+			return
+		}
+
 		start := time.Now()
 
 		// Create a custom response writer to capture response body
-		writer := &responseWriter{body: &bytes.Buffer{}, ResponseWriter: c.Writer}
-		c.Writer = writer
+		w := &responseWriter{body: &bytes.Buffer{}, ResponseWriter: c.Writer}
+		c.Writer = w
 
 		// Process request
 		c.Next()
@@ -35,14 +39,17 @@ func CustomLogger() gin.HandlerFunc {
 		// Calculate resolution time
 		duration := time.Since(start)
 
+		// Ensure we capture the status
+		status := c.Writer.Status()
+
 		// Log details of the request and response
 		logger.Info("Request processed",
 			zap.String("method", c.Request.Method),
 			zap.String("path", c.Request.URL.Path),
-			zap.Int("status", c.Writer.Status()),
+			zap.Int("status", status),
 			zap.Duration("duration", duration),
 			zap.String("clientIP", c.ClientIP()),
-			zap.String("response", writer.body.String()), // Log the response body
+			zap.String("response", w.body.String()),
 		)
 	}
 }
--- routers/main.go
+++ routers/main.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"github.com/timam/uttaracloud-finance-backend/handlers"
 	"github.com/timam/uttaracloud-finance-backend/middlewares"
 	"github.com/timam/uttaracloud-finance-backend/pkg/logger"
@@ -10,7 +11,15 @@ import (
 func InitRouter() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Recovery())
+
+	// Always apply the CustomLogger middleware
 	router.Use(middlewares.CustomLogger())
+
+	if viper.GetBool("server.debug") {
+		logger.Info("Debug mode enabled, request and response logging activated")
+	} else {
+		logger.Info("Debug mode disabled, request and response logging deactivated")
+	}
 
 	logger.Info("Initializing router")
 
