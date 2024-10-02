013e903d8b6a15c6776e4020353ecfc479218020
timam <mail@timam.io>
2024-10-03 02:08:53 +0600

    Refactor: Move db package to new directory
    Relocated the database initialization logic from `internals/db` to `pkg/db`. This change improves the organization of the codebase by properly categorizing the db package. No functionality has been altered.

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/timam/uttaracloud-finance-backend/cmd/server"
 	"github.com/timam/uttaracloud-finance-backend/internals/configs"
-	"github.com/timam/uttaracloud-finance-backend/internals/db"
 	"github.com/timam/uttaracloud-finance-backend/internals/packages"
+	"github.com/timam/uttaracloud-finance-backend/pkg/db"
 	"github.com/timam/uttaracloud-finance-backend/pkg/logger"
 	"go.uber.org/zap"
 	"os"
